270f5ce2c45c3f607c7d868b680d02ffcc3cbb93
royarrdhian <roy.ardhian@gmail.com>
2016-10-31 04:33:57 +0700

    change some css

--- dist/cust_js/welcome.js
+++ dist/cust_js/welcome.js
@@ -1,15 +1,17 @@
 function focusFunction(){
 	$("body").css("background-color","#aaa");
 	$("body").css("transition","background-color 500ms");
+	$(".form-signin").css("color","black");
 	$(".form-signin").css("background-color","#eee");
 	$(".form-signin").css("transition","background-color 500ms");
-	$(".form-signin").css("box-shadow","10px 10px 10px grey");
+	$(".form-signin").css("box-shadow","0px 15px 15px grey");
 	$(".form-signin").css("transition","box-shadow 500ms");
 }
 
 function blurFunction(){
 	$("body").css("background","#eee");
 	$("body").css("transition","background-color 500ms");
+	$(".form-signin").css("color","white");
 	$(".form-signin").css("background-color","#aaa");
 	$(".form-signin").css("transition","background-color 500ms");
 	$(".form-signin").css("box-shadow","");
